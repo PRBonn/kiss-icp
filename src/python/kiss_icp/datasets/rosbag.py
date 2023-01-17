29d8a0138e4e61626008d425cabaab91e87c5309
RoniEmad <roni.emad1@gmail.com>
2023-01-17 12:13:52 +0200

    test

--- src/python/kiss_icp/datasets/rosbag.py
+++ src/python/kiss_icp/datasets/rosbag.py
@@ -76,6 +76,7 @@ class RosbagDataset:
             timestamps = np.array(list(self.pc2.read_points(msg, field_names=t_field)))
             if t_field != "time":
                 timestamps = timestamps / np.max(timestamps)
+                "test"
         return points.astype(np.float64), timestamps
 
     def check_for_topics(self):
