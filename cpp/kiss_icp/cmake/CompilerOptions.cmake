447bb7ade0d78abc288241219fa3a0097c95d5fe
Alexander Hans <ahans@users.noreply.github.com>
2023-03-29 11:53:28 +0200

    Enable and fix warnings (#100)
    * Enable and fix warnings
    
    This enables warning flags for MSVC/gcc/clang and fixes the warnings
    reported.
    
    Co-authored-by: Ignacio Vizzo <ignaciovizzo@gmail.com>
    
    * Replace lambda by range loop
    
    ---------
    
    Co-authored-by: Ignacio Vizzo <ignaciovizzo@gmail.com>

--- cpp/kiss_icp/cmake/CompilerOptions.cmake
+++ cpp/kiss_icp/cmake/CompilerOptions.cmake
@@ -22,9 +22,29 @@
 # SOFTWARE.
 function(set_global_target_properties target)
   target_compile_features(${target} PUBLIC cxx_std_17)
-  target_compile_options(${target} PRIVATE $<$<COMPILE_LANG_AND_ID:CXX,Clang,AppleClang>:-fcolor-diagnostics>)
-  target_compile_options(${target} PRIVATE $<$<COMPILE_LANG_AND_ID:CXX,GNU>:-fdiagnostics-color=always>)
-
+  target_compile_options(
+    ${target}
+    PRIVATE # MSVC
+            $<$<COMPILE_LANG_AND_ID:CXX,MSVC>:/W4>
+            $<$<COMPILE_LANG_AND_ID:CXX,MSVC>:/WX>
+            # Clang/AppleClang
+            $<$<COMPILE_LANG_AND_ID:CXX,Clang,AppleClang>:-fcolor-diagnostics>
+            $<$<COMPILE_LANG_AND_ID:CXX,Clang,AppleClang>:-Werror>
+            $<$<COMPILE_LANG_AND_ID:CXX,Clang,AppleClang>:-Wall>
+            $<$<COMPILE_LANG_AND_ID:CXX,Clang,AppleClang>:-Wextra>
+            $<$<COMPILE_LANG_AND_ID:CXX,Clang,AppleClang>:-Wconversion>
+            $<$<COMPILE_LANG_AND_ID:CXX,Clang,AppleClang>:-Wno-sign-conversion>
+            # GNU
+            $<$<COMPILE_LANG_AND_ID:CXX,GNU>:-fdiagnostics-color=always>
+            $<$<COMPILE_LANG_AND_ID:CXX,GNU>:-Werror>
+            $<$<COMPILE_LANG_AND_ID:CXX,GNU>:-Wall>
+            $<$<COMPILE_LANG_AND_ID:CXX,GNU>:-Wextra>
+            $<$<COMPILE_LANG_AND_ID:CXX,GNU>:-pedantic>
+            $<$<COMPILE_LANG_AND_ID:CXX,GNU>:-Wcast-align>
+            $<$<COMPILE_LANG_AND_ID:CXX,GNU>:-Wcast-qual>
+            $<$<COMPILE_LANG_AND_ID:CXX,GNU>:-Wconversion>
+            $<$<COMPILE_LANG_AND_ID:CXX,GNU>:-Wdisabled-optimization>
+            $<$<COMPILE_LANG_AND_ID:CXX,GNU>:-Woverloaded-virtual>)
   set(INCLUDE_DIRS ${PROJECT_SOURCE_DIR})
   get_filename_component(INCLUDE_DIRS ${INCLUDE_DIRS} PATH)
   target_include_directories(${target} PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}
