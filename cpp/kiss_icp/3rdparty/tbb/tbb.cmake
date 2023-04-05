b814a7af5cdeb07a31c9658241cce1443a95d0b5
Alexander Hans <ahans@users.noreply.github.com>
2023-04-05 08:52:28 +0200

    Use official TBB v2021.8.0 release
    * Update custom flags for tbb build
    * Add lib64 to TBBHelper lib dirs
    
    Co-authored-by: Ignacio Vizzo <ignaciovizzo@gmail.com>

--- cpp/kiss_icp/3rdparty/tbb/tbb.cmake
+++ cpp/kiss_icp/3rdparty/tbb/tbb.cmake
@@ -26,17 +26,17 @@ find_package(Threads)
 ExternalProject_Add(
   external_tbb
   PREFIX tbb
-  URL https://github.com/nachovizzo/tbb/archive/refs/tags/tbbstatic.tar.gz
-  URL_HASH SHA256=db5ede77c4bd10ad12fab11ed38b7e8cf80aba85db16a57514073c383e6c8630
+  URL https://github.com/oneapi-src/oneTBB/archive/refs/tags/v2021.8.0.tar.gz
+  URL_HASH SHA256=eee380323bb7ce864355ed9431f85c43955faaae9e9bce35c62b372d7ffd9f8b
   UPDATE_COMMAND ""
   CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
              ${ExternalProject_CMAKE_ARGS}
              ${ExternalProject_CMAKE_CXX_FLAGS}
              # custom flags
-             -DTBB_BUILD_TBBMALLOC=ON
-             -DTBB_BUILD_SHARED=OFF
-             -DTBB_BUILD_STATIC=ON
-             -DTBB_BUILD_TESTS=OFF)
+             -DBUILD_SHARED_LIBS=OFF
+             -DTBB_EXAMPLES=OFF
+             -DTBB_STRICT=OFF
+             -DTBB_TEST=OFF)
 
 # Simulate importing TBB::tbb for OpenVDBHelper target
 ExternalProject_Get_Property(external_tbb INSTALL_DIR)
@@ -44,6 +44,6 @@ set(TBB_ROOT ${INSTALL_DIR} CACHE INTERNAL "TBB_ROOT Install directory")
 add_library(TBBHelper INTERFACE)
 add_dependencies(TBBHelper external_tbb)
 target_include_directories(TBBHelper SYSTEM INTERFACE ${INSTALL_DIR}/include)
-target_link_directories(TBBHelper INTERFACE ${INSTALL_DIR}/lib)
+target_link_directories(TBBHelper INTERFACE ${INSTALL_DIR}/lib ${INSTALL_DIR}/lib64)
 target_link_libraries(TBBHelper INTERFACE tbb Threads::Threads)
 add_library(TBB::tbb ALIAS TBBHelper)
