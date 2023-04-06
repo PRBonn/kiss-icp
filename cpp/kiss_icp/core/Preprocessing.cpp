a4387f14251e8135fe166b75c99bf88b3a2c9f72
Ignacio Vizzo <ignaciovizzo@gmail.com>
2023-04-06 15:34:30 +0200

    Attempt to fix windows build

--- cpp/kiss_icp/core/Preprocessing.cpp
+++ cpp/kiss_icp/core/Preprocessing.cpp
@@ -20,8 +20,6 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
-#define _USE_MATH_DEFINES  // fix Windows build
-
 #include "Preprocessing.hpp"
 
 #include <tbb/parallel_for.h>
