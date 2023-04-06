2f12d2f6e5e407bdca1e80a54f55538dc9c6e0a9
Ignacio Vizzo <ignaciovizzo@gmail.com>
2023-04-06 13:13:15 +0200

    Cleanup Sophus.cmake build script

--- cpp/kiss_icp/3rdparty/sophus/sophus.cmake
+++ cpp/kiss_icp/3rdparty/sophus/sophus.cmake
@@ -20,6 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 include(ExternalProject)
+include(GNUInstallDirs)
 
 ExternalProject_Add(
   external_sophus
@@ -28,16 +29,12 @@ ExternalProject_Add(
   # URL https://github.com/strasdat/Sophus/archive/refs/tags/1.22.10.tar.gz
   URL https://github.com/nachovizzo/Sophus/archive/refs/tags/1.22.11.tar.gz
   UPDATE_COMMAND ""
-  CONFIGURE_COMMAND ""
-  BUILD_COMMAND ""
-  INSTALL_COMMAND ""
-  CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> -DSOPHUS_USE_BASIC_LOGGING=ON -DBUILD_SOPHUS_EXAMPLES=OFF
-             -DBUILD_SOPHUS_TESTS=OFF -DCMAKE_BUILD_TYPE=Release)
+  CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> -DBUILD_SOPHUS_EXAMPLES=OFF -DBUILD_SOPHUS_TESTS=OFF
+             -DCMAKE_BUILD_TYPE=Release)
 
-ExternalProject_Get_Property(external_sophus SOURCE_DIR)
+ExternalProject_Get_Property(external_sophus INSTALL_DIR)
 add_library(SophusHelper INTERFACE)
 add_dependencies(SophusHelper external_sophus)
 target_compile_definitions(SophusHelper INTERFACE SOPHUS_USE_BASIC_LOGGING=1)
-target_include_directories(SophusHelper SYSTEM INTERFACE $<BUILD_INTERFACE:${SOURCE_DIR}>)
-set_property(TARGET SophusHelper PROPERTY EXPORT_NAME Sophus::Sophus)
+target_include_directories(SophusHelper SYSTEM INTERFACE ${INSTALL_DIR}/${CMAKE_INSTALL_INCLUDEDIR})
 add_library(Sophus::Sophus ALIAS SophusHelper)
