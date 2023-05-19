03ccaa4594a15a399751b2508d8d5bf77f22a155
Patrick Roncagliolo <ronca.pat@gmail.com>
2023-05-19 14:07:46 +0200

    Fixes

--- ros/ros2/OdometryServer.hpp
+++ ros/ros2/OdometryServer.hpp
@@ -79,5 +79,4 @@ private:
 // Register the component with class_loader.
 // This acts as a sort of entry point, allowing the component to be
 // discoverable when its library is being loaded into a running process.
-// is being loaded into a running process.
 RCLCPP_COMPONENTS_REGISTER_NODE(kiss_icp_ros::OdometryServer)
