5b3bfc7d2f798e583fdfa1d58d341bec1fe04a68
Ignacio Vizzo <ignaciovizzo@gmail.com>
2023-08-17 17:35:42 +0200

    Shrink cmake script and capitalize everything

--- cpp/kiss_icp/3rdparty/find_dependencies.cmake
+++ cpp/kiss_icp/3rdparty/find_dependencies.cmake
@@ -20,24 +20,13 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
 # Silence timestamp warning
 if(CMAKE_VERSION VERSION_GREATER 3.24)
   cmake_policy(SET CMP0135 OLD)
 endif()
 
 include(3rdparty/utils.cmake)
-
-# Eigen
-find_external_dependecy("USE_SYSTEM_EIGEN3" "Eigen3" "Eigen3::Eigen" "${CMAKE_CURRENT_LIST_DIR}/eigen/eigen.cmake")
-
-# Sophus
-find_external_dependecy("USE_SYSTEM_SOPHUS" "Sophus" "Sophus::Sophus" "${CMAKE_CURRENT_LIST_DIR}/sophus/sophus.cmake")
-
-# tbb needs to be statically linked, so, also do it always :)
-find_external_dependecy("USE_SYSTEM_TBB" "TBB" "TBB::tbb" "${CMAKE_CURRENT_LIST_DIR}/tbb/tbb.cmake")
-
-# tsl_robin
-
-find_external_dependecy("USE_SYSTEM_TSLMAP" "tsl-robin-map" "tsl::robin_map"
-                        "${CMAKE_CURRENT_LIST_DIR}/tsl_robin/tsl_robin.cmake")
+find_external_dependecy("Eigen3" "Eigen3::Eigen" "${CMAKE_CURRENT_LIST_DIR}/eigen/eigen.cmake")
+find_external_dependecy("Sophus" "Sophus::Sophus" "${CMAKE_CURRENT_LIST_DIR}/sophus/sophus.cmake")
+find_external_dependecy("TBB" "TBB::tbb" "${CMAKE_CURRENT_LIST_DIR}/tbb/tbb.cmake")
+find_external_dependecy("tsl-robin-map" "tsl::robin_map" "${CMAKE_CURRENT_LIST_DIR}/tsl_robin/tsl_robin.cmake")
