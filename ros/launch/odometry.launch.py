d21fdb068f036b0fcb7ba2ee9a28bdb168324cf5
Ignacio Vizzo <ignacio@dexory.com>
2024-02-29 11:35:09 +0100

    Move ros params from launch file to YAML

--- ros/launch/odometry.launch.py
+++ ros/launch/odometry.launch.py
@@ -20,7 +20,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, ExecuteProcess
 from launch.conditions import IfCondition
@@ -29,62 +28,50 @@ from launch.substitutions import (
     PathJoinSubstitution,
     PythonExpression,
 )
-from launch_ros.actions import Node
+from launch_ros.actions import Node, SetParameter
 from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
     current_pkg = FindPackageShare("kiss_icp")
+    use_sim_time = SetParameter(name="use_sim_time", value=True)
+    topic_arg = DeclareLaunchArgument(
+        "topic", description="sensor_msg/PointCloud2 topic to process"
+    )
+    bagfile_arg = DeclareLaunchArgument("bagfile", default_value="")
+    visualize_arg = DeclareLaunchArgument("visualize", default_value="true")
+
+    kiss_icp_node = Node(
+        package="kiss_icp",
+        executable="odometry_node",
+        name="odometry_node",
+        output="screen",
+        remappings=[("pointcloud_topic", LaunchConfiguration("topic"))],
+        parameters=[PathJoinSubstitution([current_pkg, "config", "kiss_icp.yaml"])],
+    )
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        output="screen",
+        arguments=[
+            "-d",
+            PathJoinSubstitution([current_pkg, "rviz", "kiss_icp.rviz"]),
+        ],
+        condition=IfCondition(LaunchConfiguration("visualize")),
+    )
+    bagfile_play = ExecuteProcess(
+        cmd=["ros2", "bag", "play", LaunchConfiguration("bagfile")],
+        output="screen",
+        condition=IfCondition(PythonExpression(["'", LaunchConfiguration("bagfile"), "' != ''"])),
+    )
     return LaunchDescription(
         [
-            # ROS 2 parameters
-            DeclareLaunchArgument("topic", description="sensor_msg/PointCloud2 topic to process"),
-            DeclareLaunchArgument("bagfile", default_value=""),
-            DeclareLaunchArgument("visualize", default_value="true"),
-            DeclareLaunchArgument("odom_frame", default_value="odom"),
-            DeclareLaunchArgument("base_frame", default_value=""),
-            DeclareLaunchArgument("publish_odom_tf", default_value="true"),
-            # KISS-ICP parameters
-            DeclareLaunchArgument("deskew", default_value="false"),
-            DeclareLaunchArgument("max_range", default_value="100.0"),
-            DeclareLaunchArgument("min_range", default_value="5.0"),
-            # This thing is still not suported: https://github.com/ros2/launch/issues/290#issuecomment-1438476902
-            #  DeclareLaunchArgument("voxel_size", default_value=None),
-            Node(
-                package="kiss_icp",
-                executable="odometry_node",
-                name="odometry_node",
-                output="screen",
-                remappings=[("pointcloud_topic", LaunchConfiguration("topic"))],
-                parameters=[
-                    {
-                        "odom_frame": LaunchConfiguration("odom_frame"),
-                        "base_frame": LaunchConfiguration("base_frame"),
-                        "max_range": LaunchConfiguration("max_range"),
-                        "min_range": LaunchConfiguration("min_range"),
-                        "deskew": LaunchConfiguration("deskew"),
-                        #  "voxel_size": LaunchConfiguration("voxel_size"),
-                        "max_points_per_voxel": 20,
-                        "initial_threshold": 2.0,
-                        "min_motion_th": 0.1,
-                        "publish_odom_tf": LaunchConfiguration("publish_odom_tf"),
-                        "visualize": LaunchConfiguration("visualize"),
-                    }
-                ],
-            ),
-            Node(
-                package="rviz2",
-                executable="rviz2",
-                output={"both": "log"},
-                arguments=["-d", PathJoinSubstitution([current_pkg, "rviz", "kiss_icp.rviz"])],
-                condition=IfCondition(LaunchConfiguration("visualize")),
-            ),
-            ExecuteProcess(
-                cmd=["ros2", "bag", "play", LaunchConfiguration("bagfile")],
-                output="screen",
-                condition=IfCondition(
-                    PythonExpression(["'", LaunchConfiguration("bagfile"), "' != ''"])
-                ),
-            ),
+            use_sim_time,
+            topic_arg,
+            bagfile_arg,
+            visualize_arg,
+            kiss_icp_node,
+            rviz_node,
+            bagfile_play,
         ]
     )
