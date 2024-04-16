b97d195df5c5575271f3eaebf5df3ebe25dbaef5
tizianoGuadagnino <frevo93@gmail.com>
2024-04-16 17:00:12 +0200

    Revert "This can be reverted, but it is related"
    This reverts commit 59b5d21607e66883ad71fa6b3ad3463075515d4a.

--- cpp/kiss_icp/3rdparty/eigen/eigen.cmake
+++ cpp/kiss_icp/3rdparty/eigen/eigen.cmake
@@ -24,6 +24,7 @@
 # TODO: Yet another manual release dne by nacho. This should be updated whenever the Eigen team
 # release a new version that is not 3.4. That version does not include this necessary changes:
 # - https://gitlab.com/libeigen/eigen/-/merge_requests/893/diffs
+
 set(EIGEN_BUILD_DOC OFF CACHE BOOL "Don't build Eigen docs")
 set(EIGEN_BUILD_TESTING OFF CACHE BOOL "Don't build Eigen tests")
 set(EIGEN_BUILD_PKGCONFIG OFF CACHE BOOL "Don't build Eigen pkg-config")
@@ -32,4 +33,15 @@ set(EIGEN_BUILD_LAPACK OFF CACHE BOOL "Don't build lapack module")
 
 include(FetchContent)
 FetchContent_Declare(eigen URL https://github.com/nachovizzo/eigen/archive/refs/tags/3.4.90.tar.gz)
-FetchContent_MakeAvailable(eigen)
+if(NOT eigen_POPULATED)
+  FetchContent_Populate(eigen)
+  if(${CMAKE_VERSION} GREATER_EQUAL 3.25)
+    add_subdirectory(${eigen_SOURCE_DIR} ${eigen_BINARY_DIR} SYSTEM EXCLUDE_FROM_ALL)
+  else()
+    # Emulate the SYSTEM flag introduced in CMake 3.25. Withouth this flag the compiler will
+    # consider this 3rdparty headers as source code and fail due the -Werror flag.
+    add_subdirectory(${eigen_SOURCE_DIR} ${eigen_BINARY_DIR} EXCLUDE_FROM_ALL)
+    get_target_property(eigen_include_dirs eigen INTERFACE_INCLUDE_DIRECTORIES)
+    set_target_properties(eigen PROPERTIES INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "${eigen_include_dirs}")
+  endif()
+endif()
--- cpp/kiss_icp/3rdparty/tbb/tbb.cmake
+++ cpp/kiss_icp/3rdparty/tbb/tbb.cmake
@@ -20,7 +20,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
+# option(BUILD_SHARED_LIBS ON)
 option(BUILD_SHARED_LIBS OFF)
 option(TBBMALLOC_BUILD OFF)
 option(TBB_EXAMPLES OFF)
@@ -29,4 +29,15 @@ option(TBB_TEST OFF)
 
 include(FetchContent)
 FetchContent_Declare(tbb URL https://github.com/oneapi-src/oneTBB/archive/refs/tags/v2021.8.0.tar.gz)
-FetchContent_MakeAvailable(tbb)
+if(NOT tbb_POPULATED)
+  FetchContent_Populate(tbb)
+  if(${CMAKE_VERSION} GREATER_EQUAL 3.25)
+    add_subdirectory(${tbb_SOURCE_DIR} ${tbb_BINARY_DIR} SYSTEM EXCLUDE_FROM_ALL)
+  else()
+    # Emulate the SYSTEM flag introduced in CMake 3.25. Withouth this flag the compiler will
+    # consider this 3rdparty headers as source code and fail due the -Werror flag.
+    add_subdirectory(${tbb_SOURCE_DIR} ${tbb_BINARY_DIR} EXCLUDE_FROM_ALL)
+    get_target_property(tbb_include_dirs tbb INTERFACE_INCLUDE_DIRECTORIES)
+    set_target_properties(tbb PROPERTIES INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "${tbb_include_dirs}")
+  endif()
+endif()
