59b5d21607e66883ad71fa6b3ad3463075515d4a
tizianoGuadagnino <frevo93@gmail.com>
2024-04-16 16:12:15 +0200

    This can be reverted, but it is related

--- cpp/kiss_icp/3rdparty/eigen/eigen.cmake
+++ cpp/kiss_icp/3rdparty/eigen/eigen.cmake
@@ -25,16 +25,18 @@
 # release a new version that is not 3.4. That version does not include this necessary changes:
 # - https://gitlab.com/libeigen/eigen/-/merge_requests/893/diffs
 
-set(EIGEN_BUILD_DOC OFF CACHE BOOL "Don't build Eigen docs")
-set(EIGEN_BUILD_TESTING OFF CACHE BOOL "Don't build Eigen tests")
-set(EIGEN_BUILD_PKGCONFIG OFF CACHE BOOL "Don't build Eigen pkg-config")
-set(EIGEN_BUILD_BLAS OFF CACHE BOOL "Don't build blas module")
-set(EIGEN_BUILD_LAPACK OFF CACHE BOOL "Don't build lapack module")
-
-include(FetchContent)
-FetchContent_Declare(eigen URL https://github.com/nachovizzo/eigen/archive/refs/tags/3.4.90.tar.gz)
+FetchContent_GetProperties(eigen)
 if(NOT eigen_POPULATED)
+  set(EIGEN_BUILD_DOC OFF CACHE BOOL "Don't build Eigen docs")
+  set(EIGEN_BUILD_TESTING OFF CACHE BOOL "Don't build Eigen tests")
+  set(EIGEN_BUILD_PKGCONFIG OFF CACHE BOOL "Don't build Eigen pkg-config")
+  set(EIGEN_BUILD_BLAS OFF CACHE BOOL "Don't build blas module")
+  set(EIGEN_BUILD_LAPACK OFF CACHE BOOL "Don't build lapack module")
+
+  include(FetchContent)
+  FetchContent_Declare(eigen URL https://github.com/nachovizzo/eigen/archive/refs/tags/3.4.90.tar.gz)
   FetchContent_Populate(eigen)
+
   if(${CMAKE_VERSION} GREATER_EQUAL 3.25)
     add_subdirectory(${eigen_SOURCE_DIR} ${eigen_BINARY_DIR} SYSTEM EXCLUDE_FROM_ALL)
   else()
