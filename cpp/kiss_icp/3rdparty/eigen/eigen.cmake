86f3f9654f29ec8349d1b515956d622cc37c933d
tizianoGuadagnino <frevo93@gmail.com>
2024-04-17 13:57:58 +0200

    Add guarded cmake version check, will validate on dummy project

--- cpp/kiss_icp/3rdparty/eigen/eigen.cmake
+++ cpp/kiss_icp/3rdparty/eigen/eigen.cmake
@@ -32,6 +32,23 @@ set(EIGEN_BUILD_BLAS OFF CACHE BOOL "Don't build blas module")
 set(EIGEN_BUILD_LAPACK OFF CACHE BOOL "Don't build lapack module")
 
 include(FetchContent)
-FetchContent_Declare(eigen URL https://github.com/nachovizzo/eigen/archive/refs/tags/3.4.90.tar.gz EXCLUDE_FROM_ALL
-                               SYSTEM)
-FetchContent_MakeAvailable(eigen)
+if(${CMAKE_VERSION} GREATER_EQUAL 3.28)
+  FetchContent_Declare(eigen URL https://github.com/nachovizzo/eigen/archive/refs/tags/3.4.90.tar.gz EXCLUDE_FROM_ALL
+                                 SYSTEM)
+  FetchContent_MakeAvailable(eigen)
+else()
+  FetchContent_Declare(eigen URL https://github.com/nachovizzo/eigen/archive/refs/tags/3.4.90.tar.gz)
+  FetchContent_GetProperties(eigen)
+  if(NOT eigen_POPULATED)
+    FetchContent_Populate(eigen)
+    if(${CMAKE_VERSION} GREATER_EQUAL 3.25)
+      add_subdirectory(${eigen_SOURCE_DIR} ${eigen_BINARY_DIR} SYSTEM EXCLUDE_FROM_ALL)
+    else()
+      # Emulate the SYSTEM flag introduced in CMake 3.25. Withouth this flag the compiler will
+      # consider this 3rdparty headers as source code and fail due the -Werror flag.
+      add_subdirectory(${eigen_SOURCE_DIR} ${eigen_BINARY_DIR} EXCLUDE_FROM_ALL)
+      get_target_property(eigen_include_dirs eigen INTERFACE_INCLUDE_DIRECTORIES)
+      set_target_properties(eigen PROPERTIES INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "${eigen_include_dirs}")
+    endif()
+  endif()
+endif()
