356fc9213439ada58c7135a3423a92a86204bdfc
Benedikt Mersch <benedikt.mersch@gmail.com>
2024-04-17 13:33:12 +0200

    For now keep this separated

--- cpp/kiss_icp/3rdparty/eigen/eigen.cmake
+++ cpp/kiss_icp/3rdparty/eigen/eigen.cmake
@@ -21,6 +21,10 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+# TODO: Yet another manual release dne by nacho. This should be updated whenever the Eigen team
+# release a new version that is not 3.4. That version does not include this necessary changes:
+# - https://gitlab.com/libeigen/eigen/-/merge_requests/893/diffs
+
 set(EIGEN_BUILD_DOC OFF CACHE BOOL "Don't build Eigen docs")
 set(EIGEN_BUILD_TESTING OFF CACHE BOOL "Don't build Eigen tests")
 set(EIGEN_BUILD_PKGCONFIG OFF CACHE BOOL "Don't build Eigen pkg-config")
@@ -28,5 +32,16 @@ set(EIGEN_BUILD_BLAS OFF CACHE BOOL "Don't build blas module")
 set(EIGEN_BUILD_LAPACK OFF CACHE BOOL "Don't build lapack module")
 
 include(FetchContent)
-FetchContent_Declare(eigen GIT_REPOSITORY https://gitlab.com/libeigen/eigen GIT_TAG 3.4 EXCLUDE_FROM_ALL SYSTEM)
-FetchContent_MakeAvailable(eigen)
+FetchContent_Declare(eigen GIT_REPOSITORY https://gitlab.com/libeigen/eigen GIT_TAG 3.4)
+if(NOT eigen_POPULATED)
+  FetchContent_Populate(eigen)
+  if(${CMAKE_VERSION} GREATER_EQUAL 3.25)
+    add_subdirectory(${eigen_SOURCE_DIR} ${eigen_BINARY_DIR} SYSTEM EXCLUDE_FROM_ALL)
+  else()
+    # Emulate the SYSTEM flag introduced in CMake 3.25. Withouth this flag the compiler will
+    # consider this 3rdparty headers as source code and fail due the -Werror flag.
+    add_subdirectory(${eigen_SOURCE_DIR} ${eigen_BINARY_DIR} EXCLUDE_FROM_ALL)
+    get_target_property(eigen_include_dirs eigen INTERFACE_INCLUDE_DIRECTORIES)
+    set_target_properties(eigen PROPERTIES INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "${eigen_include_dirs}")
+  endif()
+endif()
