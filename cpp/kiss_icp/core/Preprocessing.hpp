b43166f55f1fb032fc15fe97a2cb766a8ff732d6
Ignacio Vizzo <ignacio@dexory.com>
2024-07-24 09:31:04 +0200

    Nacho/remove kitti from core library (#369)
    * Remove kitti scan correction from core library
    
    Put it in python bindings, that is the only place where is actually used
    
    * Fix merge conflict

--- cpp/kiss_icp/core/Preprocessing.hpp
+++ cpp/kiss_icp/core/Preprocessing.hpp
@@ -31,10 +31,4 @@ namespace kiss_icp {
 std::vector<Eigen::Vector3d> Preprocess(const std::vector<Eigen::Vector3d> &frame,
                                         double max_range,
                                         double min_range);
-
-/// This function only applies for the KITTI dataset, and should NOT be used by any other dataset,
-/// the original idea and part of the implementation is taking from CT-ICP(Although IMLS-SLAM
-/// Originally introduced the calibration factor)
-std::vector<Eigen::Vector3d> CorrectKITTIScan(const std::vector<Eigen::Vector3d> &frame);
-
 }  // namespace kiss_icp
