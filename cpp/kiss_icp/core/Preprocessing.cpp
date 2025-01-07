f16f6d34708a593d3a62c34974193a729b56b559
tizianoGuadagnino <frevo93@gmail.com>
2025-01-07 16:52:44 +0100

    Now it matches main

--- cpp/kiss_icp/core/Preprocessing.cpp
+++ cpp/kiss_icp/core/Preprocessing.cpp
@@ -36,13 +36,7 @@
 #include <vector>
 
 namespace {
-constexpr double mid_pose_timestamp{0.5};
-Eigen::Vector3d deskew(const Eigen::Vector3d &point,
-                       const double stamp,
-                       const Sophus::SE3d::Tangent &motion) {
-    const auto pose = Sophus::SE3d::exp((stamp - mid_pose_timestamp) * motion);
-    return pose * point;
-}
+constexpr double mid_pose_stamp{0.5};
 }  // namespace
 
 namespace kiss_icp {
@@ -65,27 +59,36 @@ Preprocessor::Preprocessor(const double max_range,
 std::vector<Eigen::Vector3d> Preprocessor::Preprocess(const std::vector<Eigen::Vector3d> &frame,
                                                       const std::vector<double> &timestamps,
                                                       const Sophus::SE3d &relative_motion) const {
-    const bool has_to_deskew = deskew_ && !timestamps.empty();
-    const auto motion_vector = relative_motion.log();
-    tbb::concurrent_vector<Eigen::Vector3d> preprocessed_frame;
-    preprocessed_frame.reserve(frame.size());
-    tbb::parallel_for(
-        // Index Range
-        tbb::blocked_range<size_t>{0, frame.size()},
-        // Parallel Compute
-        [&](const tbb::blocked_range<size_t> &r) {
-            for (size_t idx = r.begin(); idx < r.end(); ++idx) {
-                const auto &point = frame.at(idx);
-                const auto deskewed_point =
-                    has_to_deskew ? deskew(point, timestamps.at(idx), motion_vector) : point;
-                const double point_range = deskewed_point.norm();
-                if (point_range < max_range_ && point_range > min_range_) {
-                    preprocessed_frame.emplace_back(deskewed_point);
-                }
-            };
-        });
-    std::vector<Eigen::Vector3d> returned(std::make_move_iterator(preprocessed_frame.cbegin()),  //
-                                          std::make_move_iterator(preprocessed_frame.cend()));
-    return returned;
+    const std::vector<Eigen::Vector3d> &deskewed_frame = [&]() {
+        if (!deskew_ || timestamps.empty()) {
+            return frame;
+        } else {
+            const auto motion = relative_motion.log();
+            std::vector<Eigen::Vector3d> deskewed_frame(frame.size());
+            tbb::parallel_for(
+                // Index Range
+                tbb::blocked_range<size_t>{0, deskewed_frame.size()},
+                // Parallel Compute
+                [&](const tbb::blocked_range<size_t> &r) {
+                    for (size_t idx = r.begin(); idx < r.end(); ++idx) {
+                        const auto &point = frame.at(idx);
+                        const auto &stamp = timestamps.at(idx);
+                        const auto pose = Sophus::SE3d::exp((stamp - mid_pose_stamp) * motion);
+                        deskewed_frame.at(idx) = pose * point;
+                    };
+                });
+            return deskewed_frame;
+        }
+    }();
+    std::vector<Eigen::Vector3d> preprocessed_frame;
+    preprocessed_frame.reserve(deskewed_frame.size());
+    std::for_each(deskewed_frame.cbegin(), deskewed_frame.cend(), [&](const auto &point) {
+        const double norm = point.norm();
+        if (norm < max_range_ && norm > min_range_) {
+            preprocessed_frame.emplace_back(point);
+        }
+    });
+    preprocessed_frame.shrink_to_fit();
+    return preprocessed_frame;
 }
 }  // namespace kiss_icp
