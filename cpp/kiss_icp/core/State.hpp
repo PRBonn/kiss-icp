b16fcb849955cf5a1ffefaa17a82978eedd1dbf0
tizianoGuadagnino <frevo93@gmail.com>
2025-01-10 10:31:14 +0100

    Add state

---
+++ cpp/kiss_icp/core/State.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <sophus/se3.hpp>
+
+namespace kiss_icp {
+struct State {
+    using Vector6d = Sophus::SE3d::Tangent;
+    using JacobianMatrixType = Eigen::Matrix<double, 3, 6>;
+    Sophus::SE3d poseAtNormalizedTime(const double alpha) const;
+    Vector6d velocityAtNormalizedTime(const double alpha) const;
+
+    inline void updateCoefficients(const Vector6d &dx) { acceleration_coefficient += dx; }
+
+    void computeNextState();
+
+    inline Eigen::Vector3d transformPoint(const Eigen::Vector3d &point, const double alpha) {
+        return poseAtNormalizedTime(alpha) * point;
+    }
+
+    JacobianMatrixType jacobianTransformPoint(const Eigen::Vector3d &point, const double alpha);
+
+protected:
+    Sophus::SE3d pose;
+    Vector6d velocity_coefficient = Vector6d::Zero();
+    Vector6d acceleration_coefficient = Vector6d::Zero();
+};
+}  // namespace kiss_icp
