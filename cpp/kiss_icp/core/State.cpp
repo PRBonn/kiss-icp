db9f5731d34fc4313c38ada2c696db20ea5d6fec
tizianoGuadagnino <frevo93@gmail.com>
2025-01-10 10:32:42 +0100

    Now it compiles

--- cpp/kiss_icp/core/State.cpp
+++ cpp/kiss_icp/core/State.cpp
@@ -5,13 +5,13 @@ double square(const double x) { return x * x; }
 }  // namespace
 
 namespace kiss_icp {
-Sophus::SE3d State::poseAtNormalizedTime(const double alpha) {
+Sophus::SE3d State::poseAtNormalizedTime(const double alpha) const {
     const State::Vector6d &omega =
         acceleration_coefficient * square(alpha) + velocity_coefficient * alpha;
     return pose * Sophus::SE3d::exp(omega);
 }
 
-State::Vector6d State::velocityAtNormalizedTime(const double alpha) {
+State::Vector6d State::velocityAtNormalizedTime(const double alpha) const {
     const auto Jr_inverse = Sophus::SE3d::leftJacobianInverse(-pose.log());
     return Jr_inverse * (2.0 * acceleration_coefficient * alpha + velocity_coefficient);
 }
@@ -21,7 +21,7 @@ State::JacobianMatrixType State::jacobianTransformPoint(const Eigen::Vector3d &p
     State::JacobianMatrixType J_icp = State::JacobianMatrixType::Zero();
     J_icp.block<3, 3>(0, 0) = Eigen::Matrix3d::Identity();
     J_icp.block<3, 3>(0, 3) = -Sophus::SO3d::hat(point);
-    const Eigen::Matrix3d &rotation_at_alpha = poseAtNormalizedTime(alpha).so3().rotationMatrix();
+    const Eigen::Matrix3d &rotation_at_alpha = poseAtNormalizedTime(alpha).so3().matrix();
     const State::Vector6d &omega_at_alpha =
         acceleration_coefficient * square(alpha) + velocity_coefficient * alpha;
     const auto Jr = Sophus::SE3d::leftJacobian(-omega_at_alpha);
