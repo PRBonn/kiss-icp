dee012b014435a6da281e38ac8255a2f56067175
ssg1002 <sgupta2@uni-bonn.de>
2025-06-11 10:57:55 +0200

    fix formatting ci

--- python/kiss_icp/config/parser.py
+++ python/kiss_icp/config/parser.py
@@ -67,7 +67,6 @@ def _yaml_source(config_file: Optional[Path]) -> Dict[str, Any]:
 def load_config(config_file: Optional[Path]) -> KISSConfig:
     """Load configuration from an optional yaml file."""
 
-
     config = KISSConfig(**_yaml_source(config_file))
 
     # Check if there is a possible mistake
