57387f063787d2ade0dccfc5bae8f8de1ddf0465
jimmy <cq2essz@gmail.com>
2025-10-23 16:31:41 +0800

    log update

--- ros/src/Utils.hpp
+++ ros/src/Utils.hpp
@@ -100,7 +100,7 @@ inline std::optional<PointField> GetTimestampField(const PointCloud2::ConstShare
     }
     if (timestamp_field.count) return timestamp_field;
     RCLCPP_WARN_ONCE(rclcpp::get_logger("kiss_icp_node"),
-                     "Field 't', 'timestamp', or 'time'  does not exist. "
+                     "Field 't', 'timestamp', 'time_stamp', or 'time'  does not exist. "
                      "Disabling scan deskewing");
     return {};
 }
