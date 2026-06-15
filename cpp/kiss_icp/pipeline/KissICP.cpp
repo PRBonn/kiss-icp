d10b856334aa3f03f874d07d83c9a81a6469287a
ssg1002 <sgupta2@uni-bonn.de>
2026-06-15 09:10:07 +0200

    move hessian after map update

--- cpp/kiss_icp/pipeline/KissICP.cpp
+++ cpp/kiss_icp/pipeline/KissICP.cpp
@@ -52,7 +52,6 @@ KissICP::Vector3dVectorTuple KissICP::RegisterFrame(const std::vector<Eigen::Vec
                                                          initial_guess,  // initial_guess
                                                          3.0 * sigma,    // max_correspondence_dist
                                                          sigma);         // kernel
-    last_hessian_ = registration_.GetHessian(source, local_map_, new_pose, 3.0 * sigma);
 
     // Compute the difference between the prediction and the actual estimate
     const auto model_deviation = initial_guess.inverse() * new_pose;
@@ -63,6 +62,8 @@ KissICP::Vector3dVectorTuple KissICP::RegisterFrame(const std::vector<Eigen::Vec
     last_delta_ = last_pose_.inverse() * new_pose;
     last_pose_ = new_pose;
 
+    last_hessian_ = registration_.GetHessian(source, local_map_, last_pose_,
+                                             3.0 * adaptive_threshold_.ComputeThreshold());
     // Return the (deskew) input raw scan (preprocessed_frame) and the points used for registration
     // (source)
     return {std::move(preprocessed_frame), std::move(source)};
